62f6cd48cc956b27a709eae1ea707e9887ef1a68
kardioversion1 <evan.kuhl@gmail.com>
2025-08-15 09:40:23 -0400

    chore: remove legacy artifacts

--- state.js
+++ state.js
@@ -1,16 +1,13 @@
 
-+import { todayISO } from './utils.js';
-+
-+export const STATE = {
-+  date: todayISO(),
-+  shift: 'day',
-+  locked: true,
-+  zones: ['Beds 1–3','Beds 4–7','Beds 8–10','Fast Track'],
-+  staff: [],
-+  pin: '4911'
-+};
-+
-+export const getById = id => STATE.staff.find(s=>s.id===id);
- 
-EOF
-)
+import { todayISO } from './utils.js';
+
+export const STATE = {
+  date: todayISO(),
+  shift: 'day',
+  locked: true,
+  zones: ['Beds 1–3','Beds 4–7','Beds 8–10','Fast Track'],
+  staff: [],
+  pin: '4911'
+};
+
+export const getById = id => STATE.staff.find(s=>s.id===id);
